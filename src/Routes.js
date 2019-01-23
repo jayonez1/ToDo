50282581815800b72f75467457c55a7ee4def30e
Vladislav <jayone.makevlad@gmail.com>
2019-01-23 11:58:30 +0500

    Fix: Routes Link

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,74 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import { Link } from 'react-router-dom';
+
 import App from './containers/App';
 import WeekPage from './containers/WeekPage';
 import SecondPage from './containers/SecondPage';
 
-export default () => (
-  <App>
-    <div>
-      <Switch>
-        <Route path="/second" component={SecondPage} />
-        <Route path="/" component={WeekPage} />
-      </Switch>
-    </div>
-  </App>
-);
+
+import routes from "./constants/routes.json"
+
+import {
+  Layout, Menu, Breadcrumb, Icon,
+} from 'antd';
+
+const {
+  Header, Content, Footer, Sider,
+} = Layout;
+const SubMenu = Menu.SubMenu;
+
+class Routers extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = (collapsed) => {
+    this.setState({ collapsed });
+  }
+
+  render() {
+    return (
+      <App>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}
+          >
+            <Menu theme="dark" mode="inline">
+              <Menu.Item key="Лист дел">
+                <Link to={routes.LIST}>
+                  <Icon type="profile" />
+                  <span>Лист дел</span>
+                </Link>
+              </Menu.Item>
+              <SubMenu
+                key="sub1"
+                title={<span><Icon type="calendar" /><span>Календарь</span></span>}
+              >
+                <Menu.Item key="Неделя"><Link to={routes.WEEK}>Неделя</Link></Menu.Item>
+                <Menu.Item key="Месяц"><Link to={routes.MONTH}>Месяц</Link></Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout>
+
+            <Content style={{ margin: '16px 16px' }}>
+              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                <Switch>
+                  <Route path={routes.WEEK} component={WeekPage} />
+                </Switch>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+
+            </Footer>
+          </Layout>
+        </Layout>
+      </App>
+    );
+  }
+}
+
+export default Routers
--- src/index.js
+++ src/index.js
@@ -4,73 +4,13 @@ import * as serviceWorker from './serviceWorker';
 
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
-
-import {
-  Layout, Menu, Breadcrumb, Icon,
-} from 'antd';
+import "./index.css"
 import 'antd/dist/antd.css';
-
 
 const store = configureStore();
 
-serviceWorker.unregister();
-
-
-const {
-  Header, Content, Footer, Sider,
-} = Layout;
-const SubMenu = Menu.SubMenu;
-
-class ToDoMain extends React.Component {
-  state = {
-    collapsed: false,
-  };
-
-  onCollapse = (collapsed) => {
-    this.setState({ collapsed });
-  }
-
-  render() {
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline">
-            <Menu.Item key="1">
-              <Icon type="profile" />
-              <span>Лист дел</span>
-            </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="calendar" /><span>Календарь</span></span>}
-            >
-              <Menu.Item key="2">Неделя</Menu.Item>
-              <Menu.Item key="3">Месяц</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              <Root store={store} history={history} />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
-        </Layout>
-      </Layout>
-    );
-  }
-}
-
 ReactDOM.render(
-  <ToDoMain />
+  <Root store={store} history={history} />
   , document.getElementById('root'));
+
+serviceWorker.unregister();
